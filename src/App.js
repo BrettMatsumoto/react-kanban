adf91cf94eaeade272ed989f7b7be38a3b465d64
brett <brett_matsumoto@yahoo.com>
2019-05-15 18:38:26 -1000

    set template for App.js and added components/containers

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AddCard from './containers/AddCard';
+import CardList from './containers/CardList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: 'Kanban Board',
+    };
+  }
+
+  componentDidMount() {
+    return this.props.loadCards();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <CardListTitle title={this.state.title} />
+        </header>
+
+        <div className="cardlist-container">
+          <CardList cards={this.props.cards} />
+        </div>
+
+        <div className="add-card-form">
+          <AddCard />
+        </div>
+      </div>
+    );
+  }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    cards: state.cardReducer.cards,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    loadCards: () => {
+      return dispatch(loadCards());
+    },
+  };
+};
+
+App = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(App);
 
 export default App;
